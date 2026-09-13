6ac1f02e795c0a23df301dbf9d5c27477005ac23
mushee <musheelabs@proton.me>
2026-09-13 05:35:21 +0100

    revert: remove unused signed-evidence client wrapper

--- src/components/InstitutionalPolicyPanelClientOnly.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const InstitutionalPolicyPanel = dynamic(() => import("./InstitutionalPolicyPanel"), { ssr: false });
-
-export default function InstitutionalPolicyPanelClientOnly() {
-  return <InstitutionalPolicyPanel />;
-}
